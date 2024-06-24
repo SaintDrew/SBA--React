fb30b3708e41dfb37e084b6aefcd6446a757ccde
SaintDrew <drew_el12@yahoo.com>
2024-06-24 07:12:00 -0400

    fixing context

--- react-website/src/components/Booklist/Booklist.jsx
+++ react-website/src/components/Booklist/Booklist.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { useGlobalContext } from '../../context';
+import { useGlobalContext } from '../../context.';
 import Book from "../BookList/Book";
 import Loading from "../Loader/Loader";
 import coverImg from "../../images/book-not-found.png";
-import "./BookList";
+import "./BookList.css";
 
 //https://covers.openlibrary.org/b/id/240727-S.jpg
 
@@ -34,10 +34,10 @@ const BookList = () => {
               )
             })
           }
-          </div>
         </div>
-      </section>
-    )
-  }
-  
-  export default BookList
+      </div>
+    </section>
+  )
+}
+
+export default BookList
